28635b216acd7e3bd42cef749a43ccfb285fe16e
unknown <hans.cruz.0011@gmail.com>
2020-12-02 22:54:55 -0300

    test views y forms

--- criticasHC/peliculas/test/test_models.py
+++ criticasHC/peliculas/test/test_models.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from peliculas.models import Movie 
 class PruebaM(TestCase):
 	@classmethod
@@ -26,4 +26,22 @@ class PruebaM(TestCase):
 		movie=Movie.objects.get(id='7c32217e-7347-4bf5-82d7-3f71d48d2ad7')
 		field_label = Movie._meta.get_field('clasif').verbose_name
 		self.assertEquals(field_label,'clasif')
+
+class ViewTest(TestCase):
+	def test_peliculas(self):
+		c= Client()
+		resp=c.get('/peliculas/')
+		self.assertEqual(resp.status_code, 200)
+	def test_criticas(self):
+		o= Client()
+		resp=o.get('/peliculas/criticas/')
+		self.assertEqual(resp.status_code, 200)
+	def test_quienes(self):
+		co= Client()
+		resp=co.get('/peliculas/quienes/')
+		self.assertEqual(resp.status_code, 200)
+	def test_InicioSesion(self):
+		i= Client()
+		resp=i.get('/accounts/login/')
+		self.assertEqual(resp.status_code, 200)
 
